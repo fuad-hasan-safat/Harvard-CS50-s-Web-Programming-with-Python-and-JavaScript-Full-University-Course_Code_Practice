3d3d8c1a4318aaccc592196301ff854a15339018
fuadsaft <fuadsafat16@gmail.com>
2023-07-18 08:24:53 +0600

    adding two additional urls

--- Django/lecture3/hello/urls.py
+++ Django/lecture3/hello/urls.py
@@ -5,5 +5,7 @@ from . import views # dot(.) reffers current directory
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("hasan", views.hasan, name="hasan"),
+    path("safat", views.safat, name="safat")
 ]
--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request): # this function takes http request and return httpRespond
     return HttpResponse("Hello World!")
+
+def hasan(request):
+    return HttpResponse("Hello, Hasan!")
+
+def safat(request):
+    return HttpResponse("Hello, Safat!")
