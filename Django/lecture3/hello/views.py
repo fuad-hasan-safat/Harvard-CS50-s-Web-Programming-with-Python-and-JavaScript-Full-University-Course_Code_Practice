54a44c805791be03ade085c3559a75b00d937f29
fuadsaft <fuadsafat16@gmail.com>
2023-07-18 08:07:20 +0600

    adding urls on main path

--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request): # this function takes http request and return httpRespond
+    return HttpResponse("Hello World!")
