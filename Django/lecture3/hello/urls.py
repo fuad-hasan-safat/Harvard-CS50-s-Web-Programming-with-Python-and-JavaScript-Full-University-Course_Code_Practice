89a549c8be08e1b8f5070549977523205d3bd6a0
fuadsaft <fuadsafat16@gmail.com>
2023-07-19 17:17:32 +0600

    adding html templates

--- Django/lecture3/hello/urls.py
+++ Django/lecture3/hello/urls.py
@@ -6,6 +6,7 @@ from . import views # dot(.) reffers current directory
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("hasan", views.hasan, name="hasan"),
     path("safat", views.safat, name="safat")
 ]
--- Django/lecture3/hello/views.py
+++ Django/lecture3/hello/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): # this function takes http request and return httpRespond
-    return HttpResponse("Hello World!")
+    return render(request,"hello/index.html")
 
 def hasan(request):
     return HttpResponse("Hello, Hasan!")
 
 def safat(request):
     return HttpResponse("Hello, Safat!")
+
+def greet(request, name):
+    return render(request,"hello/greet.html",{
+        "name": name.capitalize()
+    })
